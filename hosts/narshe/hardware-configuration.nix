c7420b1bfe63e26bac9acdad81646454e7cbe0f0
Nadia Holmquist Pedersen <nadia@nhp.sh>
2024-07-24 22:27:42 +0200

    remove unused filesystem stuff

--- hosts/narshe/hardware-configuration.nix
+++ hosts/narshe/hardware-configuration.nix
@@ -15,19 +15,6 @@
     zenpower
   ];
 
-  #fileSystems."/" =
-  #  { device = "/dev/disk/by-uuid/acedbb0c-b63c-4f28-9d7e-7c1bd3e49e52";
-  #    fsType = "ext4";
-  #  };
-
-  #fileSystems."/boot" =
-  #  { device = "/dev/disk/by-uuid/4645-B34E";
-  #    fsType = "vfat";
-  #    options = [ "fmask=0022" "dmask=0022" ];
-  #  };
-
-  #swapDevices = [ ];
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
