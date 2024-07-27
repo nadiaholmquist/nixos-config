1b7182b1427cd34800f9103a4f1fd2468a2f550f
Nadia Holmquist Pedersen <nadia@nhp.sh>
2024-07-27 23:39:49 +0200

    install sponserblock

--- home/firefox.nix
+++ home/firefox.nix
@@ -1,6 +1,14 @@
 { pkgs, lib, ... }:
 
-lib.mkIf pkgs.stdenv.isLinux {
+let
+  inherit (lib.attrsets) mapAttrs' nameValuePair;
+
+  mkExtensions = exts: mapAttrs' (name: id:
+    nameValuePair id {
+      install_url = "https://addons.mozilla.org/firefox/downloads/latest/${name}/latest.xpi";
+      installation_mode = "force_installed";
+    }) exts;
+in lib.mkIf pkgs.stdenv.isLinux {
   programs.firefox = {
     enable = true;
     nativeMessagingHosts = with pkgs; [ kdePackages.plasma-browser-integration ];
@@ -9,28 +17,16 @@ lib.mkIf pkgs.stdenv.isLinux {
       DisableFirefoxStudies = true;
       DisablePocket = true;
       PasswordManagerEnabled = false; # I don't use the Firefox password manager
-      ExtensionSettings = {
-        "uBlock0@raymondhill.net" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = { # Bitwarden
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        "gdpr@cavi.au.dk" = { # Consent-o-Matic
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/consent-o-matic/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        "avi6106@gmail.com" = { # YouTube Auto HD + FPS
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/youtube-auto-hd-fps/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        "plasma-browser-integration@kde.org" = { # KDE integration
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/plasma-integration/latest.xpi";
-          installation_mode = "force_installed";
-        };
+
+      ExtensionSettings = mkExtensions {
+        ublock-origin = "uBlock0@raymondhill.net";
+        bitwarden-password-manager = "{446900e4-71c2-419f-a6a7-df9c091e268b}";
+        consent-o-matic = "gdpr@cavi.au.dk";
+        youtube-auto-hd-fps = "avi6106@gmail.com";
+        plasma-integration = "plasma-browser-integration@kde.org";
+        sponsorblock = "sponsorBlocker@ajay.app";
       };
+
       Preferences = {
         "browser.tabs.closeWindowWithLastTab" = false;
         "widget.use-xdg-desktop-portal.file-picker" = 1; # Use KDE file picker
