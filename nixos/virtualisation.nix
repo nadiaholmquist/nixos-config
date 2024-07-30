414983a7a65c5c079c18367c2ab575e7a88dc86a
Nadia Holmquist Pedersen <nadia@nhp.sh>
2024-07-30 19:25:40 +0200

    update virtualisation stuff

--- nixos/virtualisation.nix
+++ nixos/virtualisation.nix
@@ -3,14 +3,24 @@
 {
   # libvirt
   virtualisation.libvirtd.enable = lib.mkDefault true;
+  virtualisation.libvirtd.qemu.swtpm.enable = lib.mkDefault true;
+  virtualisation.libvirtd.qemu.ovmf.packages = with pkgs; [
+    (OVMF.override {
+      secureBoot = true;
+      msVarsTemplate = true;
+      tpmSupport = true;
+    }).fd
+  ];
+
   programs.virt-manager.enable = lib.mkDefault true;
+
+  specialisation.vmware.configuration = { pkgs, ... }: {
+    boot.kernelPackages = lib.mkForce pkgs.linuxPackages;
+    virtualisation.vmware.host.enable = true;
+  };
 
   # Not Docker
   virtualisation.podman.enable = true;
   virtualisation.podman.dockerSocket.enable = true;
   virtualisation.podman.dockerCompat = true;
-
-  environment.systemPackages = with pkgs; [
-    podman-compose
-  ];
 }
