7ec46a2d6499270795099d673de433dd7ff38948
Nadia Holmquist Pedersen <nadia@nhp.sh>
2024-08-02 04:08:28 +0200

    home: basic appearance settings
    mostly just to fix the cursor theme

---
+++ home/appearance.nix
@@ -0,0 +1,14 @@
+{ pkgs, lib, ... }:
+
+lib.mkIf pkgs.stdenv.isLinux {
+  # For some reason, setting this through Home Manager fixes the cursor theme being inconsistent in some apps (notablly, JetBrains IDEs and Steam).
+  # just setting it in KDE doesn't work.
+  # I can even omit the package below and have it make an invalid symlink and it'll still work, very weird...
+  home.pointerCursor = {
+    gtk.enable = true;
+    package = pkgs.kdePackages.breeze;
+    name = "breeze_cursors";
+  };
+
+  gtk.theme.name = "Breeze";
+}
--- home/default.nix
+++ home/default.nix
@@ -35,5 +35,6 @@
     ./neovim.nix
     ./firefox.nix
     ./openal-soft.nix
+    ./appearance.nix
   ];
 }
