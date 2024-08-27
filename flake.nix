f7dcf3abfa62890d52ca040c333bd77bb9c05c27
Nadia Holmquist Pedersen <nadia@nhp.sh>
2024-08-27 15:37:53 +0200

    move flake boilerplate to a separate file

--- flake.nix
+++ flake.nix
@@ -33,62 +33,8 @@
     };
   };
 
-  outputs = { nixpkgs, darwin, disko, home-manager, ... } @inputs: let
-    #unstablePkgsFor = system: final: prev: rec {
-    #  pkgsUnstable = import nixpkgs-unstable {
-    #    inherit system;
-    #    config.allowUnfree = true;
-    #  };
-    #};
-
-    commonModuleFor = system: hostName: let
-      #unstableOverlay = unstablePkgsFor system;
-    in {
-      nix.settings.experimental-features = "nix-command flakes repl-flake";
-      networking.hostName = hostName;
-
-      nixpkgs.config.allowUnfree = true;
-      #nixpkgs.overlays = [ unstableOverlay ];
-    };
-
-    hmModule = {
-      home-manager.useGlobalPkgs = true;
-      home-manager.useUserPackages = true;
-      home-manager.sharedModules = [
-        { _module.args = { inherit inputs; }; }
-      ];
-      home-manager.users.nhp = import ./home;
-    };
-
-    nixos = { hostName, system, extra ? {} }: let
-      commonModule = commonModuleFor system hostName;
-    in nixpkgs.lib.nixosSystem {
-      system = system;
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./nixos
-        ./hosts/${hostName}
-        disko.nixosModules.disko
-        home-manager.nixosModules.home-manager
-        commonModule
-        hmModule
-      ];
-    } // extra;
-
-    macos = { hostName, extra ? {} }: let
-      system = "aarch64-darwin";
-      commonModule = commonModuleFor system hostName;
-    in darwin.lib.darwinSystem {
-      inherit system;
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./darwin
-        ./hosts/${hostName}
-        home-manager.darwinModules.home-manager
-        commonModule
-        hmModule
-      ];
-    } // extra;
+  outputs = { nixpkgs, home-manager, ... } @inputs: let
+    inherit (import ./lib/hosts.nix inputs) nixos macos home;
   in {
     nixosConfigurations = {
       narshe = nixos {
@@ -110,13 +56,9 @@
     };
 
     homeConfigurations = {
-      deck = home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs { system = "x86_64-linux"; };
-        extraSpecialArgs = { inherit inputs; };
-        modules = [
-          ./hosts/deck
-          ./home
-        ];
+      deck = home {
+        hostName = "deck";
+        system = "x86_64-linux";
       };
     };
   };
---
+++ lib/hosts.nix
@@ -0,0 +1,68 @@
+{ nixpkgs, home-manager, darwin, disko, ... } @inputs:
+
+let
+  #unstablePkgsFor = system: final: prev: rec {
+  #  pkgsUnstable = import nixpkgs-unstable {
+  #    inherit system;
+  #    config.allowUnfree = true;
+  #  };
+  #};
+
+  commonModuleFor = system: hostName: let
+    #unstableOverlay = unstablePkgsFor system;
+  in {
+    nix.settings.experimental-features = "nix-command flakes repl-flake";
+    networking.hostName = hostName;
+
+    nixpkgs.config.allowUnfree = true;
+    #nixpkgs.overlays = [ unstableOverlay ];
+  };
+
+  hmModule = {
+    home-manager.useGlobalPkgs = true;
+    home-manager.useUserPackages = true;
+    home-manager.sharedModules = [
+      { _module.args = { inherit inputs; }; }
+    ];
+    home-manager.users.nhp = import ../home;
+  };
+in {
+  nixos = { hostName, system, extra ? {} }: let
+    commonModule = commonModuleFor system hostName;
+  in nixpkgs.lib.nixosSystem {
+    system = system;
+    specialArgs = { inherit inputs; };
+    modules = [
+      ../nixos
+      ../hosts/${hostName}
+      disko.nixosModules.disko
+      home-manager.nixosModules.home-manager
+      commonModule
+      hmModule
+    ];
+  } // extra;
+
+  macos = { hostName, extra ? {} }: let
+    system = "aarch64-darwin";
+    commonModule = commonModuleFor system hostName;
+  in darwin.lib.darwinSystem {
+    inherit system;
+    specialArgs = { inherit inputs; };
+    modules = [
+      ../darwin
+      ../hosts/${hostName}
+      home-manager.darwinModules.home-manager
+      commonModule
+      hmModule
+    ];
+  } // extra;
+
+  home = { hostName, system }: home-manager.lib.homeManagerConfiguration {
+    pkgs = import nixpkgs { inherit system; };
+    extraSpecialArgs = { inherit inputs; };
+    modules = [
+      ../hosts/${hostName}
+      ../home
+    ];
+  };
+}
