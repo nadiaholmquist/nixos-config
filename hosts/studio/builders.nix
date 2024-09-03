dae193c63820779d5f2e322f6cb225814b673ff5
Nadia Holmquist Pedersen <nadia@nhp.sh>
2024-09-04 01:34:51 +0200

    studio: use build VMs

--- hosts/studio/builders.nix
+++ hosts/studio/builders.nix
@@ -4,7 +4,7 @@
   nix.settings.builders-use-substitutes = true;
   nix.distributedBuilds = true;
   nix.buildMachines = [
-    {
+    /*{
       hostName = "narshe.local";
       systems = [ "x86_64-linux" "i686-linux" ];
       supportedFeatures = [ "kvm" "big-parallel" "nixos-test" "benchmark" ];
@@ -12,6 +12,24 @@
       sshUser = "nhp";
       protocol = "ssh-ng";
       maxJobs = 16;
+    }*/
+    {
+      hostName = "build01";
+      systems = [ "x86_64-linux" ];
+      supportedFeatures = ["kvm" "big-parallel" "nixos-test" "benchmark"];
+      sshUser = "ops";
+      publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUZIc2xMMUlUOFBEME91czVONmQrQ2lzclJLZjJybGROYWxGVmpOeTBHNDIgcm9vdEBidWlsZDAxCg==";
+      protocol = "ssh-ng";
+      maxJobs = 12;
+    }
+    {
+      hostName = "build02";
+      systems = [ "x86_64-linux" ];
+      supportedFeatures = ["kvm" "big-parallel" "nixos-test" "benchmark"];
+      sshUser = "ops";
+      publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUxySFhvZVpzVHI4Y0tOT1ZSQ2FLOEpsbkFsOEh6dWk4R2Zuc2NZV3gyRjggcm9vdEBidWlsZDAyCg==";
+      protocol = "ssh-ng";
+      maxJobs = 12;
     }
   ];
 }
