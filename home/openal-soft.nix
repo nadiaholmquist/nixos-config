d471890d069ea33bed1c3fcf14e2bc89823a6b35
Nadia Holmquist Pedersen <nadia@nhp.sh>
2024-10-17 12:02:00 +0200

    openal: try to make sure the HRTF actually gets used

--- home/openal-soft.nix
+++ home/openal-soft.nix
@@ -1,13 +1,14 @@
-{ config, ... }:
+{ config, lib, ... }:
 
-let
-  xdgConfig = config.xdg.configHome;
-in{
-  xdg.configFile."alsoft.conf".text = ''
-    [General]
-    default-hrtf=IRC_1058_48000
-    hrtf-paths="${xdgConfig}/alsoft/hrtf,"
-  '';
+{
+  xdg.configFile."alsoft.conf".text = lib.generators.toINI {} {
+    General = {
+      default-hrtf = "IRC_1058_48000";
+      frequency = 48000;
+      hrtf-paths = "${config.xdg.configHome}/alsoft/hrtf,";
+      stereo-mode = "headphones";
+    };
+  };
 
   xdg.configFile."alsoft/hrtf/IRC_1058_44100.mhr".source = ./static/IRC_1058_44100.mhr;
   xdg.configFile."alsoft/hrtf/IRC_1058_48000.mhr".source = ./static/IRC_1058_48000.mhr;
