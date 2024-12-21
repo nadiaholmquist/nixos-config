a7986c4360408be5845a0736dc42fda1128b48b9
Nadia Holmquist Pedersen <nadia@nhp.sh>
2024-12-21 13:08:56 +0100

    get rid of neovide override, no longer needed

--- nixos/overlay.nix
+++ nixos/overlay.nix
@@ -1,12 +1,8 @@
-{ pkgs, lib, ... }:
+{ ... }:
 
 {
   nixpkgs.overlays = [
     (final: prev: {
-      # Temporary workaround of unconditionally included cctools which only builds on Darwin
-      neovide = prev.neovide.override {
-        cctools.libtool = pkgs.runCommandNoCC "empty-drv" {} "mkdir $out";
-      };
     })
   ];
 }
