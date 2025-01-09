f31255b21be984a45ee8144e2e3eedf033aec760
Nadia Holmquist Pedersen <nadia@nhp.sh>
2025-01-09 19:55:30 +0100

    add packages for HDR output
    ...I can't get gamescope to work for Steam games though even with all
    those dependencies added to the steam env, none of the solutions I can
    find worked. Too bad I guess :(

--- nixos/gaming.nix
+++ nixos/gaming.nix
@@ -8,12 +8,33 @@ in {
   };
 
   config = lib.mkIf haveSteam {
+    environment.systemPackages = [
+      (pkgs.callPackage ../pkgs/vk-hdr-layer.nix {})
+    ];
+
     programs.steam = {
       enable = true;
+      package = pkgs.steam.override {
+        extraPkgs = pkgs: with pkgs; [
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          libkrb5
+          keyutils
+        ];
+      };
       extest.enable = true; # Steam Input on Wayland
       gamescopeSession.enable = true;
       extraCompatPackages = [
         pkgs.proton-ge-bin
+      ];
+      extraPackages = with pkgs; [
+        gamescope
       ];
     };
 
