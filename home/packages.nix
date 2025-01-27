9c3c0610c599c3fda0157ca283a74b25fdeb17dd
Nadia Holmquist Pedersen <nadia@nhp.sh>
2025-01-27 13:36:23 +0100

    make the setup of hm gui apps a bit nicer

--- home/packages.nix
+++ home/packages.nix
@@ -1,73 +1,68 @@
-{ config, lib, pkgs, ... }:
+{ lib, pkgs, ... }:
 
 let
-  inherit (lib) optionals;
+  inherit (lib) optionals concatLists;
   inherit (pkgs.hostPlatform) isx86_64 isLinux isDarwin;
-
-  isx86Linux = isLinux && isx86_64;
-
-  commonPackages = with pkgs; [
-    # CLI utilities
-    fastfetch # you've gotta have a fetch program, right?
-    gh
-    ripgrep
-    htop
-    btop
-    tmux
-    calc
-    ncdu
-
-    # Archives
-    zip
-    p7zip
-    unzip
-    unrar
-    gzip
-    lz4
-    xz
-    zstd
-
-    # Nix stuff
-    nixpkgs-fmt
-    nix-index
-    nix-search-cli
-    nix-tree
-    nix-update
-    nix-output-monitor
-    nixos-rebuild
-    comma
-    deploy-rs
-
-    # Dev CLI
-    ninja
-  ];
-
-  macPackages = optionals isDarwin (with pkgs; [
-    gnumake
-    cmake
-  ]);
-
-  linuxx86Packages = optionals isx86Linux (with pkgs; [
-    # Swift language server. Technically available on ARM Linux but requires compiling Swift and that's slow.
-    #sourcekit-lsp
-  ]);
-
-  linuxPackages = optionals isLinux (with pkgs; [
-    # CLI utilities
-    wl-clipboard
-    usbutils
-    pciutils
-    distrobox
-
-    # Development
-    stdenv.cc
-    cmakeWithGui
-    gdb
-    lldb
-  ]);
 in {
-  home.packages = commonPackages
-    ++ macPackages
-    ++ linuxx86Packages
-    ++ linuxPackages;
+  home.packages = with pkgs; concatLists [
+    [
+      # CLI utilities
+      fastfetch # you've gotta have a fetch program, right?
+      gh
+      ripgrep
+      htop
+      btop
+      tmux
+      calc
+      ncdu
+
+      # Archives
+      zip
+      p7zip
+      unzip
+      unrar
+      gzip
+      lz4
+      xz
+      zstd
+
+      # Nix stuff
+      nixpkgs-fmt
+      nix-index
+      nix-search-cli
+      nix-tree
+      nix-update
+      nix-output-monitor
+      nixos-rebuild
+      comma
+      deploy-rs
+
+      # Dev CLI
+      ninja
+    ]
+
+    (optionals isDarwin [
+      gnumake
+      cmake
+    ])
+
+    (optionals (isLinux && isx86_64) [
+      # Swift language server. Technically available on ARM Linux but requires compiling Swift and that's slow.
+      #sourcekit-lsp
+    ])
+
+    (optionals isLinux [
+      # CLI utilities
+      wl-clipboard
+      usbutils
+      pciutils
+      distrobox
+
+      # Development
+      stdenv.cc
+      cmakeWithGui
+      gdb
+      lldb
+    ])
+  ];
 }
