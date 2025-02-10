601c91da1be110c34d435f71e6366d7aecb82809
Nadia Holmquist Pedersen <nadia@nhp.sh>
2025-02-10 20:42:34 +0100

    remove minizip override
    though overrides for sfml and dolphin are still needed :(

--- overlays/clang-downgrade.nix
+++ overlays/clang-downgrade.nix
@@ -1,12 +1,6 @@
 {
   targets = ["darwin"];
   overlay = final: prev: {
-    minizip-ng = prev.minizip-ng.overrideAttrs {
-      postPatch = ''
-        substituteInPlace compat/crypt.h \
-          --replace-fail register ""
-      '';
-    };
     sfml = prev.sfml.override { stdenv = final.clang18Stdenv; };
     dolphin-emu = prev.dolphin-emu.override { stdenv = final.clang18Stdenv; };
   };
