b9de5a345b71ddb4bd3e0e8d81bf7a018691a5d4
Nadia Holmquist Pedersen <nadia@nhp.sh>
2025-02-12 22:28:04 +0100

    fix up how openal soft is set up a bit

--- home/openal-soft.nix
+++ home/openal-soft.nix
@@ -1,15 +1,18 @@
-{ config, lib, ... }:
+{ pkgs, lib, ... }:
 
+let
+  hrtfs = pkgs.runCommandNoCC "alsoft-hrtfs-dir" { } ''
+    cp -r "${./static/hrtfs}" $out
+  '';
+
+in
 {
   xdg.configFile."alsoft.conf".text = lib.generators.toINI { } {
     General = {
       default-hrtf = "IRC_1058_48000";
       frequency = 48000;
-      hrtf-paths = "${config.xdg.configHome}/alsoft/hrtf,";
+      hrtf-paths = "${hrtfs},";
       stereo-mode = "headphones";
     };
   };
-
-  xdg.configFile."alsoft/hrtf/IRC_1058_44100.mhr".source = ./static/IRC_1058_44100.mhr;
-  xdg.configFile."alsoft/hrtf/IRC_1058_48000.mhr".source = ./static/IRC_1058_48000.mhr;
 }
