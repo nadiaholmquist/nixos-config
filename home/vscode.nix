3bb56776e7fe2767c463fc4047cb43c51b45a992
Nadia Holmquist Pedersen <nadia@nhp.sh>
2025-02-26 17:32:05 +0100

    vscode: switch to vscode-fhs to make setting up language servers easier

--- home/vscode.nix
+++ home/vscode.nix
@@ -7,11 +7,11 @@
 
 lib.mkIf config.dotfiles.enableHomeGuiApps {
   programs.vscode.enable = true;
-  programs.vscode.extensions = with pkgs.vscode-extensions; [
-    vscodevim.vim
-    jnoortheen.nix-ide
-    llvm-vs-code-extensions.vscode-clangd
-    ms-vscode.cmake-tools
-    catppuccin.catppuccin-vsc
-  ];
+  programs.vscode.package = pkgs.vscode-fhsWithPackages (
+    p: with p; [
+      clang-tools
+      nixd
+      rust-analyzer
+    ]
+  );
 }
