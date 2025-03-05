3a035c508dda71b3aa3930a237c0ee14df2c0341
Nadia Holmquist Pedersen <nadia@nhp.sh>
2025-03-05 14:20:04 +0100

    try to remove the high-res scroll disable for the mouse, maybe it works better now?

--- nixos/hardware.nix
+++ nixos/hardware.nix
@@ -57,23 +57,6 @@ in
       # Logitech
       hardware.logitech.wireless.enable = true;
       hardware.logitech.wireless.enableGraphical = true;
-
-      environment.etc = {
-        # High-resolution scrolling on Linux is too janky to use because of the weird deadzone behavior that I can't find a way to disable.
-        # So just disable it entirely for now :(
-        "libinput/local-overrides.quirks".text = ''
-          [Logitech MX Master 3]
-          MatchVendor=0x46D
-          MatchProduct=0x4082
-          AttrEventCode=-REL_WHEEL_HI_RES;-REL_HWHEEL_HI_RES;
-
-          # MX Master 3 has a different PID on bluetooth
-          [Logitech MX Master 3]
-          MatchVendor=0x46D
-          MatchProduct=0xB023
-          AttrEventCode=-REL_WHEEL_HI_RES;-REL_HWHEEL_HI_RES
-        '';
-      };
     }
 
     # AMD GPU specific settings
