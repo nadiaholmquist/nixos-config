deafd2b2ba3161cb3d89bb5f35c065a72c83a67d
Nadia Holmquist Pedersen <nadia@nhp.sh>
2025-04-19 05:29:06 +0200

    enable usbmuxd for tethering with the iPhone

--- nixos/services.nix
+++ nixos/services.nix
@@ -1,13 +1,22 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  # Enable the OpenSSH daemon.
-  services.openssh = {
-    enable = true;
-    settings = {
-      X11Forwarding = true;
-      PasswordAuthentication = false;
-      PermitRootLogin = "no";
+  services = {
+    # Enable the OpenSSH daemon.
+    openssh = {
+      enable = true;
+      settings = {
+        X11Forwarding = true;
+        PasswordAuthentication = false;
+        PermitRootLogin = "no";
+      };
+    };
+
+    # USB multiplexing daemon for iOS devices
+    # makes tethering and file transfers and such work
+    usbmuxd = {
+      enable = true;
+      package = pkgs.usbmuxd2;
     };
   };
 }
