2731c070ec651d7f89061a67c7f057677f746421
Nadia Holmquist Pedersen <nadia@nhp.sh>
2025-06-23 05:55:34 +0200

    revert to LTS kernel, current latest has display corruption and audio drops out after sleep

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -19,7 +19,7 @@
 
   config = {
     boot = {
-      kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+      #kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
 
       loader = {
         timeout = 15;
